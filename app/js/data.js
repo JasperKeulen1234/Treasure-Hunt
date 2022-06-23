d956a079ef4f3ebea823cd1d79c4d352e8fee573
Maarten-de-Groot <56153329+Maarten-de-Groot@users.noreply.github.com>
2022-06-23 11:02:02 +0200

    Initial Commit

---
+++ app/js/data.js
@@ -0,0 +1,52 @@
+data = {
+    "testUitleg":{
+        "html":`
+        <ion-card>
+            <ion-card-header>
+                <ion-card-title>
+                    Test uitleg
+                </ion-card-title>
+            </ion-card-header>
+            <ion-card-content>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </ion-card-content>
+        </ion-card>`
+    },
+    "testFilm":{
+        "html":`
+        <iframe style="width: 100vw;aspect-ratio:4/4;" src="https://player.ntr.nl/index.php?id=WO_NTR_16264536" frameborder="0" allow="encrypted-media; geolocation" allowfullscreen=""></iframe>
+        `
+    },
+    "romeinen1":{
+        "html":`
+        <ion-card>
+            <ion-card-header>
+                <ion-card-subtitle>
+                    <span style="color: #00E069">Makkelijk</span>
+                </ion-card-subtitle>
+                <ion-card-title>
+                    Het Romeinse Rijk
+                </ion-card-title>
+            </ion-card-header>
+            <ion-card-content>
+                Het Romeinse Rijk was het rijk van de Romeinen. Het was één van de grootste rijken van de Oudheid. De Romeinen hadden een enorme invloed op de cultuur van Europa. De hoofdstad van het Romeinse Rijk was Rome. Tegenwoordig is Rome de hoofstad van Italië
+            </ion-card-content>
+        </ion-card>`
+    },
+    "testMultipleChoiceQuiz":{
+        "html":`
+        <h1 style="text-align:center;">Wat was de hoofdstad van het romeinse rijk? </h1>
+        <section style="display:flex;align-items:center;justify-content:space-between;padding:5rem;">
+            <div style="display:flex;flex-direction:column;align-items:center;flex-wrap:wrap">
+                <a>Rome</a>
+                <a>Antiochië</a>
+                <a>Ephesus</a>
+                <a>Carthago</a>
+            </div>
+            <div>
+                <img style="max-height: 25rem" src="./images/romanflag.png">
+            </div>
+        </section>
+        `
+    }
+}
